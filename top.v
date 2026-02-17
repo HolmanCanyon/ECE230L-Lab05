9218efb17ef02d0088175a5cd8d83d562baea7af
HolmanCanyon <canyonholman@u.boisestate.edu>
2026-02-17 10:05:39 -0700

    Update connections for circuit_a and circuit_b

--- top.v
+++ top.v
@@ -2,16 +2,18 @@ module top(
     input [6:0]sw,
     output [1:0]led
 );
+    wire a_out;
+    
     circuit_a a_inst(
         .A(sw[0]),
         .B(sw[1]),
         .C(sw[2]),
         .D(sw[3]),
-        .Y(led[0])
+        .Y(a_out)
     );
 
     circuit_b b_inst(
-        .A(a_inst.Y),
+        .A(a_out),
         .B(sw[4]),
         .C(sw[5]),
         .D(sw[6]),
