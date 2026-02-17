91ee3d63d93d397b7eda8dc3930b9c1bf834f784
HolmanCanyon <canyonholman@u.boisestate.edu>
2026-02-17 09:35:20 -0700

    Instantiate circuit_a module in top.v

--- top.v
+++ top.v
@@ -2,5 +2,11 @@ module top(
     input [6:0]sw,
     output [1:0]led
 );
-
+    circuit_a a_inst(
+        .A(sw[0]),
+        .B(sw[1]),
+        .C(sw[2])
+        .D(sw[3])
+        .Y(led[0])
+    );
 endmodule
